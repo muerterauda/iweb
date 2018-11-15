f702f1afc5b52e2d12d9a1fa62d55765182b315b
televiones <gapriser@gmail.com>
2018-11-15 11:52:42 +0100

    Modulos medio hacer

--- iweb-war/src/java/bean/ModuloCampaniaBean.java
+++ iweb-war/src/java/bean/ModuloCampaniaBean.java
@@ -5,8 +5,14 @@
  */
 package bean;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.xml.ws.WebServiceRef;
+import service.Campaña;
+import service.Modulo;
+import service.ServiciosIweb_Service;
 
 /**
  *
@@ -16,10 +22,87 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped 
 public class ModuloCampaniaBean {
 
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/ServiciosIweb/ServiciosIweb.wsdl")
+    private ServiciosIweb_Service service;
+    
+    private List<Modulo> modulos;
+    private List<Campaña> campaniasModulo;
+    
+    private boolean campVisible;
+    private boolean selected;
+
     /**
      * Creates a new instance of ModuloCampaniaBean
      */
     public ModuloCampaniaBean() {
     }
+    
+    @PostConstruct
+    public void init (){
+        modulos = getModulosService();
+        campVisible = false;
+        selected = false;
+    }
+
+    public List<Modulo> getModulos() {
+        return modulos;
+    }
+
+    public void setModulos(List<Modulo> modulos) {
+        this.modulos = modulos;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public List<Campaña> getCampaniasModulo() {
+        return campaniasModulo;
+    }
+
+    public void setCampaniasModulo(List<Campaña> campaniasModulo) {
+        this.campaniasModulo = campaniasModulo;
+    }
+
+    public boolean isCampVisible() {
+        return campVisible;
+    }
+
+    public void setCampVisible(boolean campVisible) {
+        this.campVisible = campVisible;
+    }
+
+    public String editarModulo(long id){
+        return "editarModulo";
+    }
+    
+    public void computeCampanias(long id){
+        campaniasModulo = buscarCampañasModulo(id);
+        if(campaniasModulo!=null){
+            campVisible = true;
+        }
+    }
+    
+    /* -------------------------  Servicios ---------------------------------------------------------------------------------*/
+
+    private java.util.List<service.Modulo> getModulosService() {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        service.ServiciosIweb port = service.getServiciosIwebPort();
+        return port.getModulos();
+    }
+
+    private java.util.List<service.Campaña> buscarCampañasModulo(long id) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        service.ServiciosIweb port = service.getServiciosIwebPort();
+        return port.buscarCampañasModulo(id);
+    }
+    
+    
     
 }
